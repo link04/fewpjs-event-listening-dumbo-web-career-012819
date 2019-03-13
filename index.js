4143afe6e5cf17b40616a5d414b1dd0103514819
Maximo Bautista <max.bautista97@gmail.com>
2019-03-13 15:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function addingEventListener(eventName, functionName) {
-
-}
-
-
-const main = document.getElementById('main');
+  const main = document.getElementById('main');
 
 main.addEventListener('click', event => {
   alert('I was clicked!');
 });
+
+}
+
+
