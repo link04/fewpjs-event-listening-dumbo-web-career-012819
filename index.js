a3160582496977b85e1ddb0b82125c0d8ec05941
Maximo Bautista <max.bautista97@gmail.com>
2019-03-13 15:42:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function addingEventListener() {
 
 }
+
+  
+  const main = document.getElementById('main');
+ 
+  main.addEventListener('click', event => {
+    alert('I was clicked!');
+  });
+  
